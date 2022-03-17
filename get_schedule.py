d16a894c6943180c971117c0745f619252d7826b
fabiocfabini <fabiolucas.carneiro@gmail.com>
2022-03-17 00:25:31 +0000

    remove unused import

--- get_schedule.py
+++ get_schedule.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import  WebDriverWait
-from webdriver_manager.chrome import ChromeDriverManager
 from Classes import Event
 
 PATH = r".\chromedriver.exe" # Path to the WebDriver executable
